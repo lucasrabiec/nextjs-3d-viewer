3b112ee02a376a2901d85903fb7ca58369973f30
Lukasz <lukaszrabiec@gmail.com>
2022-05-09 12:42:59 +0200

    added ring

--- components/model-viewer/model-viewer.tsx
+++ components/model-viewer/model-viewer.tsx
@@ -1,18 +1,18 @@
-import {Environment, OrbitControls, useGLTF} from "@react-three/drei";
-import {Canvas, PrimitiveProps} from "@react-three/fiber";
+import { Environment, OrbitControls, useGLTF } from "@react-three/drei";
+import { Canvas, PrimitiveProps } from "@react-three/fiber";
 
 type ModelProps = Omit<PrimitiveProps, 'object'>
 
 function Model(props: ModelProps) {
-  const { scene } = useGLTF('/dog.glb')
+  const { scene } = useGLTF('/ring.glb')
   return <primitive object={scene} {...props} />
 }
 
 export function ModelViewer() {
   return (
-    <Canvas camera={{ position: [2.5, 2.5, -2.5], fov: 25 }}>
+    <Canvas>
       <ambientLight intensity={0.5} />
-      <Model position={[-0.1, -0.2, 0]} rotation={[0, Math.PI / 2, 0]} scale={0.15} />
+      <Model scale={80}/>
       <Environment preset="studio" />
       <OrbitControls enableZoom={false}/>
     </Canvas>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
-import {ModelViewer} from "../components/model-viewer/model-viewer";
+import { ModelViewer } from "../components/model-viewer/model-viewer";
 
 const Home: NextPage = () => {
   return (
